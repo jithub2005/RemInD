e1adb7648d6806509bbdff2bc4a38b1a58f54add
Peter Wölflingseder <p.woelflingseder@j-it.at>
2014-01-07 13:45:57 +0100

    Task #1521: index handling in PaginationHelper fixed

--- remind-web/src/main/java/at/jit/remind/web/rest/PaginationHelper.java
+++ remind-web/src/main/java/at/jit/remind/web/rest/PaginationHelper.java
@@ -9,19 +9,19 @@ public class PaginationHelper
 
 	public PaginationHelper(int index, int count, int maxOnPage)
 	{
-		if (index < 0)
+		if (index < 0 || count < 0)
 		{
-			throw new IllegalArgumentException("index must not be negative");
+			throw new IllegalArgumentException("index and count must not be negative");
 		}
 
-		if (count <= 0 || maxOnPage <= 0)
+		if (maxOnPage <= 0)
 		{
-			throw new IllegalArgumentException("count and maxOnPage must be positive");
+			throw new IllegalArgumentException("maxOnPage must be positive");
 		}
 
-		normalizedIndex = (Math.min(index, count) / maxOnPage) * maxOnPage;
+		normalizedIndex = (Math.min(index, count - 1) / maxOnPage) * maxOnPage;
 		previousIndex = Math.max(normalizedIndex - maxOnPage, 0);
-		lastIndex = (count / maxOnPage) * maxOnPage;
+		lastIndex = ((count - 1) / maxOnPage) * maxOnPage;
 		nextIndex = Math.min(normalizedIndex + maxOnPage, lastIndex);
 	}
 
